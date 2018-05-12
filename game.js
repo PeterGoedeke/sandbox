ba7fe24adf5ea2e85be9b6f991c659e7b9826f85
PeterGoedeke <peterbgoedeke@gmail.com>
2018-05-13 02:21:50 +1200

    Update
    Added Params to createSpaceShip and renamed Managers to Handlers

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var MasterCollisionManager = {
+var MasterCollisionHandler = {
     collidables: [],
     registerCollidable: function(collidable) {
         this.collidables.push(collidable);
@@ -8,19 +8,31 @@ var MasterCollisionManager = {
         for(let i = 0; i < this.collidables.length; i++) {
             for(let j = 1; j < this.collidables.length && i != j; j++) {
                 if(this.collidables[i].test == "original" && this.collidables[j].test == "other") {
-                    this.collidables[i].collisionManager.respond(this.collidables[j]);
+                    this.collidables[i].collisionHandler.respond(this.collidables[j]);
                 }
             }
         }
     }
 }
 
-var CollisionManagerProto = {
+var CollisionHandlerProto = {
     init: function() {
-       MasterCollisionManager.registerCollidable(this);
+       MasterCollisionHandler.registerCollidable(this);
     },
     respond: function(other) {
-        console.log(other.test);
+        console.log(other.width);
+    }
+}
+
+var ExhaustHandlerProto = {
+    init: function() {
+
+    },
+    update: function() {
+
+    },
+    render: function() {
+
     }
 }
 
@@ -28,22 +40,29 @@ var SpaceShipProto = {
     test: "original"
 }
 
-var addCollisionManager = function(superEntity) {
-    var collisionManager = Object.create(CollisionManagerProto);
-    collisionManager.init = collisionManager.init.bind(superEntity);
-    collisionManager.respond = collisionManager.respond.bind(superEntity);
-    superEntity.collisionManager = collisionManager;
-} 
-
-var createSpaceShip = function() {
-    var SpaceShip = Object.create(SpaceShipProto);
-    addCollisionManager(SpaceShip);
-    SpaceShip.collisionManager.init();
-    return SpaceShip;
+var addCollisionHandler = function(superEntity) {
+    var collisionHandler = Object.create(CollisionHandlerProto);
+    for(const key in collisionHandler) collisionHandler[key] = collisionHandler[key].bind(superEntity);
+    superEntity.collisionHandler = collisionHandler;
 }
 
-var spaceShip = createSpaceShip();
-var otherSpaceShip = createSpaceShip();
+var addExhaustHandler = function(superEntity) {
+    var exhaustHandler = Object.create(ExhaustHandlerProto);
+    for(key in exhaustHandler) key.bind(superEntity);
+}
+
+var createSpaceShip = function(x, y, width, height) {
+    return function() {
+        var SpaceShip = Object.create(SpaceShipProto);
+        Object.assign(SpaceShip, {x, y, width, height});
+        addCollisionHandler(SpaceShip);
+        SpaceShip.collisionHandler.init();
+        return SpaceShip;
+    }
+}
+
+var spaceShip = createSpaceShip(50, 50, 15, 35)();
+var otherSpaceShip = createSpaceShip(50, 50, 50, 50)();
 otherSpaceShip.test = "other";
 
-MasterCollisionManager.checkForCollision();
+MasterCollisionHandler.checkForCollision();
