6ea8cfb3adecbcb4fa9e2434f1ef19b0d602cee6
PeterGoedeke <peterbgoedeke@gmail.com>
2018-05-13 20:24:43 +1200

    Added Display for Spaceships

--- Spaceships.js
+++ Spaceships.js
@@ -1,9 +1,18 @@
 var SpaceShipProto = {
+    element: document.createElement("div"),
     init: function init() {
         Game.registerGameObject(this);
+        this.element.classList.add("gameObject");
+        this.element.style.width = this.width + "px";
+        this.element.style.height = this.height + "px";
+        var displayImage = document.createElement("img");
+        displayImage.src = this.image;
+        displayImage.classList.add("shipDisplayGraphic");
+        this.element.appendChild(displayImage);
+        document.querySelector(".game").appendChild(this.element);
     },
     update: function update() {
-        console.log("ayy");
+        
     },
     render: function render() {
 
@@ -24,7 +33,7 @@ var createSpaceShipType = function(x, y, width, height, image, mass, baseArmour,
     }
 }
 
-var createTestSpaceShip = createSpaceShipType(0, 0, 25, 25, "player.png", 50, "placeholder", 0.02, 5, 2);
+var createTestSpaceShip = createSpaceShipType(0, 0, 30, 70, "player.png", 50, "placeholder", 0.02, 5, 2);
 
 var mySpaceShip = createTestSpaceShip({armour: 5, weapons: "myspaceship", shield: 5, inventory: 5, devices: 5}, testCollisionHandler);
 var myOtherSpaceShip = createTestSpaceShip({armour: 5, weapons: "myotherspaceship", shield: 5, inventory: 5, devices: 5}, testCollisionHandler);
